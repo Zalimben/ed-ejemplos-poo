086d86af947a86648a4c1fa052f5d5001c7c1746
Derlis Zarate <dzaratepy@gmail.com>
2020-08-11 23:07:40 -0400

    Ejemplo herencia, encapsulamiento y polimorfismo

---
+++ src/main/java/edatos/ejemplosPOO/paq1/Dibujable.java
@@ -0,0 +1,14 @@
+package edatos.ejemplosPOO.paq1;
+
+/**
+ * Adaptación de la clase de ejemplo POO
+ * publicada por el Prof. Claudio Barua en el aula virtual
+ * de Est. de Datos, FP-UNA
+ * 
+ * @author dezeta
+ *
+ */
+public interface Dibujable {
+	public void dibujar();
+
+}
---
+++ src/main/java/edatos/ejemplosPOO/paq3/Prueba.java
@@ -0,0 +1,52 @@
+package edatos.ejemplosPOO.paq3;
+
+import edatos.ejemplosPOO.paq1.*;
+import edatos.ejemplosPOO.paq2.*;
+
+/**
+ * Adaptación de la clase de ejemplo POO
+ * publicada por el Prof. Claudio Barua en el aula virtual
+ * de Est. de Datos, FP-UNA
+ * 
+ * @author dezeta
+ *
+ */
+public class Prueba {
+
+	private static void procesarFigura(Figura f) {
+		System.out.print(f.getForma() + "->");
+		System.out.println("Area " + f.calcularArea());
+		f.dibujar();
+		System.out.println();
+	}
+
+	private static void procesarDibujable(Dibujable d) {
+		System.out.println("->Dibujable");
+		d.dibujar();
+		System.out.println();
+	}
+
+	public static void main(String[] a) {
+		//Se generan 2 objetos especificos e imprimen en pantalla
+		Circulo  c1 = new Circulo(10);
+		Cuadrado c2 = new Cuadrado(10);
+
+		procesarFigura(c1);
+		procesarFigura(c2);
+		
+		//Tambien se puede usar como Dibujable
+		procesarDibujable(c1);
+		procesarDibujable(c2);
+		
+		//Polimorfismo y enlace dinamico.. F es una variable polimorfica
+		System.out.println("---- Probando el Polimorfismo y Enlace Dinamico -----");
+		Figura f;
+		int moneda = 1 + (int) (Math.random() * 99);
+		if (moneda < 50) {
+			f = new Circulo(10);
+		} else {
+			f = new Cuadrado(10);
+		}
+		procesarFigura(f);
+	}
+}
